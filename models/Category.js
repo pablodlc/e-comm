49f4c8a73d48b821d294f9e10193f0fa70b65d82
pablo de la cruz <pablodlc@gmail.com>
2021-07-21 20:51:47 -0700

    finished assignment

--- models/Category.js
+++ models/Category.js
@@ -9,13 +9,13 @@ Category.init(
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull = false,
-      primaryKey = true,
-      autoIncrement = true
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull = false
+      allowNull: false
     }
   },
   {
--- models/Product.js
+++ models/Product.js
@@ -34,7 +34,7 @@ Product.init(
       validate: {
         isNumeric: true
       }
-    }
+    },
   },
   {
     sequelize,
